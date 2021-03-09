da674ab2786034eb9305f0a9783a498c2d2b0803
SANG NGUYEN CAO <1821050830@student.humg.edu.vn>
2021-03-09 08:28:58 +0700

    Comment

---
+++ PTPMQL/Models/QuanLiHangGui.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTPMQL.Models
+{
+    public class QuanLiHangGui
+    {
+
+        public string NgayGioGuiHang { get; set; }
+        public string CanNangHangGui { get; set; }
+    }
+}
